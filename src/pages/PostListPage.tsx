55d8220a16fe5a6a5ae70e0dbedeb5b31a54336a
jeetul <zeetul00@gmail.com>
2022-04-10 09:22:48 +0900

    오타 수정 및 icon-component 제작

--- src/pages/PostListPage.tsx
+++ src/pages/PostListPage.tsx
@@ -9,53 +9,34 @@ import { IoIosSearch } from "react-icons/io";
 const PostListPage = () => {
   return (
     <div>
-      {/* <Link to="/"> */}
-      {/* </Link> */}
-      <div className="div icon-total">
+      <Link to="/login">
         <div className="div icon-area">
-          <div className="div wrapper">
-            <div className="div content">
-              <IoIosSearch className="icon color" />
-            </div>
+          <div className="div icon-centering">
+            <IoIosSearch className="img icon-coloring" />
+          </div>
+          <div className="div icon-centering">
+            <img
+              className="img icon-coloring"
+              src={search_icon}
+              alt="search-icon"
+            />
+          </div>
+          <div className="div icon-centering">
+            <img
+              className="img icon-coloring"
+              src={sample_logo_heart}
+              alt="heart-icon"
+            />
+          </div>
+          <div className="div icon-centering">
+            <img
+              className="img icon-coloring"
+              src={LogoDrHiluluk}
+              alt="logo-icon"
+            />
           </div>
         </div>
-        <div className="div icon-area">
-          <div className="div wrapper">
-            <div className="div content">
-              <img
-                className="img icon"
-                src={search_icon}
-                alt="search-icon"
-                //height={user_height}
-              />
-            </div>
-          </div>
-        </div>
-        <div className="div icon-area">
-          <div className="div wrapper">
-            <div className="div content">
-              <img
-                className="img icon"
-                src={sample_logo_heart}
-                alt="heart-icon"
-                //height={user_height}
-              />
-            </div>
-          </div>
-        </div>
-        <div className="div icon-area">
-          <div className="div wrapper">
-            <div className="div content">
-              <img
-                className="img icon"
-                src={LogoDrHiluluk}
-                alt="home-icon"
-                //height={user_height}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+      </Link>
       <div className="div color">Home1</div>
       <div className="div color">Home2</div>
     </div>
