360e50c6c014c560f79f384e2f88040dbda0037d
jeetul <zeetul00@gmail.com>
2022-04-21 19:47:02 +0900

    ctgr 수정 중

---
+++ src/components/common/CategoryTab/CategoryTab.tsx
@@ -0,0 +1,256 @@
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Responsive } from "../Responsive";
+import {
+  IoBody,
+  IoHappy,
+  IoHeart,
+  IoSad,
+  IoMic,
+  IoPerson,
+  IoShirt,
+  IoMaleFemaleSharp,
+  IoHandLeft,
+  IoFootsteps,
+  IoSkull,
+  IoWater,
+} from "react-icons/io5";
+import "./CategoryTab.scss";
+
+const CategoryTab = () => {
+  const list = document.querySelectorAll(".list");
+  const activeLink = (e: string) => {
+    const ctgrNum: HTMLElement | null = document.getElementById(e);
+    const isActive = () => {
+      for (let i = 0; i < list.length; i++) {
+        let j: HTMLElement | null = document.getElementById(String(i + 1));
+        j && (j.className = "list");
+      }
+    };
+    // ctgrNum.className === "list" :닫혀있는 상태
+    if (ctgrNum && ctgrNum.className === "list") {
+      isActive();
+      ctgrNum.className = "list active";
+    } else if (ctgrNum && ctgrNum.className === "list active") {
+      isActive();
+    }
+  };
+
+  console.log(list);
+  //const [active, setActive] = useState("");
+  const [active, setActive] = useState([
+    { id: 1, isActive: false },
+    { id: 2, isActive: false },
+    { id: 3, isActive: false },
+    { id: 4, isActive: false },
+    { id: 5, isActive: false },
+    { id: 6, isActive: false },
+    { id: 7, isActive: false },
+    { id: 8, isActive: false },
+    { id: 9, isActive: false },
+    { id: 10, isActive: false },
+    { id: 11, isActive: false },
+    { id: 12, isActive: false },
+  ]);
+
+  const [inputs, setInputs] = useState({
+    id: "",
+    isActive: "",
+  });
+  const { id, isActive } = inputs;
+
+  const handleButtonClick = (btn: number) => {
+    //const { targetId, targetIsAcive } = e.target;
+    //btn--;
+    console.log(btn);
+    setActive(
+      active.map((btn) =>
+        btn.id === active[btn.id].id ? { ...btn, isActive: true } : btn,
+      ),
+    );
+  };
+  console.log(active);
+  return (
+    <div className="ctgr-area">
+      <Responsive className="ctgr-wrapper">
+        <div className="ctgr">
+          <ul>
+            <li
+              id="1"
+              className={active[0].isActive ? "list active" : "list"}
+              onClick={() => handleButtonClick(0)}
+              // onClick={() => {
+              //   activeLink("1");
+              // }}
+            >
+              <Link to="/">
+                <span className="icon">
+                  <IoBody />
+                </span>
+                <span className="text">전신</span>
+              </Link>
+            </li>
+            <li
+              id="2"
+              className="list"
+              onClick={() => {
+                activeLink("2");
+              }}
+            >
+              <Link to="/">
+                <span className="icon">
+                  <IoHappy />
+                </span>
+                <span className="text">머리</span>
+              </Link>
+            </li>
+            <li
+              id="3"
+              className="list"
+              onClick={() => {
+                activeLink("3");
+              }}
+            >
+              <Link to="/">
+                <span className="icon">
+                  <IoHeart />
+                </span>
+                <span className="text">정신</span>
+              </Link>
+            </li>
+            <li
+              id="4"
+              className="list"
+              onClick={() => {
+                activeLink("4");
+              }}
+            >
+              <Link to="/">
+                <span className="icon">
+                  <IoSad />
+                </span>
+                <span className="text">얼굴</span>
+              </Link>
+            </li>
+            <li
+              id="5"
+              className="list"
+              onClick={() => {
+                activeLink("5");
+              }}
+            >
+              <Link to="/">
+                <span className="icon">
+                  <IoMic />
+                </span>
+                <span className="text">목</span>
+              </Link>
+            </li>
+            <li
+              id="6"
+              className="list"
+              onClick={() => {
+                activeLink("6");
+              }}
+            >
+              <Link to="/">
+                <span className="icon">
+                  <IoPerson />
+                </span>
+                <span className="text">가슴/등</span>
+              </Link>
+            </li>
+            <li
+              id="7"
+              className="list"
+              onClick={() => {
+                activeLink("7");
+              }}
+            >
+              <Link to="/">
+                <span className="icon">
+                  <IoShirt />
+                </span>
+                <span className="text">복부</span>
+              </Link>
+            </li>
+            <li
+              id="8"
+              className="list"
+              onClick={() => {
+                activeLink("8");
+              }}
+            >
+              <Link to="/">
+                <span className="icon">
+                  <IoMaleFemaleSharp />
+                </span>
+                <span className="text">비뇨기</span>
+              </Link>
+            </li>
+            <li
+              id="9"
+              className="list"
+              onClick={() => {
+                activeLink("9");
+              }}
+            >
+              <Link to="/">
+                <span className="icon">
+                  <IoHandLeft />
+                </span>
+                <span className="text">팔과 손</span>
+              </Link>
+            </li>
+            <li
+              id="10"
+              className="list"
+              onClick={() => {
+                activeLink("10");
+              }}
+            >
+              <Link to="/">
+                <span className="icon">
+                  <IoFootsteps />
+                </span>
+                <span className="text">다리와 발</span>
+              </Link>
+            </li>
+            <li
+              id="11"
+              className="list"
+              onClick={() => {
+                activeLink("11");
+              }}
+            >
+              <Link to="/">
+                <span className="icon">
+                  <IoSkull />
+                </span>
+                <span className="text">뼈/근육</span>
+              </Link>
+            </li>
+            <li
+              id="12"
+              className="list"
+              onClick={() => {
+                activeLink("12");
+              }}
+            >
+              <Link to="/">
+                <span className="icon">
+                  <IoWater />
+                </span>
+                <span className="text">피부</span>
+              </Link>
+            </li>
+            <div id="indic" className="indicator" />
+          </ul>
+        </div>
+      </Responsive>
+    </div>
+  );
+};
+
+export default CategoryTab;
--- src/pages/PostListPage.tsx
+++ src/pages/PostListPage.tsx
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 import "../components/common/common.scss";
 import { IoIosSearch } from "react-icons/io";
 import { HeaderContainer } from "../components/containers/common/HeaderContainer";
+import CategoryTab from "../components/common/CategoryTab/CategoryTab";
 
 const PostListPage = () => {
   return (
     <>
       <HeaderContainer />
+      <CategoryTab />
       <div>안녕하세요.</div>
     </>
   );
