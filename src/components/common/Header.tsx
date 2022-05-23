c143a275232e1f6253ca39b66ebd06da5d388fcb
jeetul <zeetul00@gmail.com>
2022-05-24 03:06:05 +0900

    common.scss에서 header.scss 분리 / user-profile-button 생성

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -4,6 +4,8 @@ import { Responsive } from "./Responsive";
 import { Link } from "react-router-dom";
 import { userInitialStateType } from "../../modules/user.type";
 import { logo } from "../../static/svg";
+import "./Header.scss";
+import { IoPersonSharp, IoReorderThree } from "react-icons/io5";
 
 interface headerType {
   user?: userInitialStateType["user"];
@@ -21,12 +23,23 @@ export const Header = ({ user, onLogout }: headerType) => {
           {user?.name ? (
             <div className="right">
               <div className="user-info">{user.nickname}</div>
-              <Button cyan fullWidth onClick={onLogout}>
-                로그아웃
-              </Button>
+              <button className="user-profile">
+                <div className="user-profile-navi">
+                  <IoReorderThree />
+                </div>
+                <div className="user-profile-icon">
+                  <IoPersonSharp />
+                </div>
+              </button>
+              <div>
+                <Button cyan fullWidth onClick={onLogout}>
+                  로그아웃
+                </Button>
+              </div>
             </div>
           ) : (
             <div className="right">
+              {/* 닉네임 위치 임시 */}
               <div className="user-info">Nick</div>
               <Button to="/login" cyan fullWidth>
                 로그인
