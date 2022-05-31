1728e2c7621dd014e4f5c73cfe6fbca11172106f
Chickenbumps <whgotjd46@gmail.com>
2022-06-01 02:37:27 +0900

    Update PostList.tsx

--- src/components/posts/PostList.tsx
+++ src/components/posts/PostList.tsx
@@ -36,7 +36,7 @@ const PostList = ({ postList, postListError, loading, isUser }: any) => {
   return (
     <div className="post-list_block">
       <div className="post-list_write-button-wrapper">
-        {isUser.nickname && (
+        {isUser?.nickname && (
           <Button cyan to="/write">
             새 글 작성하기
           </Button>
