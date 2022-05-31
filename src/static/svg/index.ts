ede67514fd063d512034092b489903049afe0b40
Chickenbumps <whgotjd46@gmail.com>
2022-06-01 02:26:25 +0900

    svg 경로 설정

--- src/static/svg/index.ts
+++ src/static/svg/index.ts
@@ -3,3 +3,6 @@ export { default as logo } from "./LogoDrHiluluk.svg";
 export { default as googleLogin } from "./GoogleLoginbtn.svg";
 export { default as kakaoLogin } from "./KakaoLoginbtn.svg";
 export { default as crashScreenLogo } from "./undraw_page_crash.svg";
+export { default as defaultThumbnail } from "./undraw_male_avatar.svg";
+export { default as addIcon } from "./add_icon.svg";
+export { default as subtractIcon } from "./subtract_icon.svg";
