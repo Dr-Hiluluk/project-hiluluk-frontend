5be8cef74b52b137e35ee97509f0d55e47af43fb
Chickenbumps <whgotjd46@gmail.com>
2022-06-03 01:50:59 +0900

    Style: "제거 readChildCommentList type"
    - readChildCommentList type 제거

--- src/modules/comment.type.ts
+++ src/modules/comment.type.ts
@@ -8,12 +8,6 @@ export const READ_COMMENT_LIST_SUCCESS =
   "comment/READ_COMMENT_LIST_SUCCESS" as const;
 export const READ_COMMENT_LIST_FAILURE =
   "comment/READ_COMMENT_LIST_FAILURE" as const;
-export const READ_CHILD_COMMENT_LIST =
-  "comment/READ_CHILD_COMMENT_LIST" as const;
-export const READ_CHILD_COMMENT_LIST_SUCCESS =
-  "comment/READ_CHILD_COMMENT_LIST_SUCCESS" as const;
-export const READ_CHILD_COMMENT_LIST_FAILURE =
-  "comment/READ_CHILD_COMMENT_LIST_FAILURE" as const;
 export const UPDATE_COMMENT = "comment/UPDATE_COMMENT" as const;
 export const UPDATE_COMMENT_SUCCESS = "comment/UPDATE_COMMENT_SUCCESS" as const;
 export const UPDATE_COMMENT_FAILURE = "comment/UPDATE_COMMENT_FAILURE" as const;
@@ -26,8 +20,6 @@ export interface commentInitialState {
   commentError: any;
   commentList: comment[];
   commentListError: any;
-  childCommentList: any;
-  childCommentListError: any;
 }
 
 export interface readCommentListSuccessDispatch {
@@ -41,20 +33,6 @@ export interface readCommentListFailureDispatch {
   type: typeof READ_COMMENT_LIST_FAILURE;
   payload: {
     commentListError: any;
-  };
-}
-
-export interface readChildCommentListSuccessDispatch {
-  type: typeof READ_CHILD_COMMENT_LIST_SUCCESS;
-  payload: {
-    childCommentList: any;
-  };
-}
-
-export interface readChildCommentListFailureDispatch {
-  type: typeof READ_CHILD_COMMENT_LIST_FAILURE;
-  payload: {
-    childCommentListError: any;
   };
 }
 
@@ -103,8 +81,6 @@ export interface updateCommentFailureDispatch {
 export type CommentDispatchType =
   | readCommentListSuccessDispatch
   | readCommentListFailureDispatch
-  | readChildCommentListSuccessDispatch
-  | readChildCommentListFailureDispatch
   | createCommentSuccessDispatch
   | createCommentFailureDispatch
   | deleteCommentSuccessDispatch
