8017e75f2435158fd10c0512c63c46149556f31e
Chickenbumps <whgotjd46@gmail.com>
2022-06-03 17:48:25 +0900

    Feat: "구현 useUserId"
    - 로그인한 userId 얻어오는 hook 구현

--- src/lib/hooks/useUser.ts
+++ src/lib/hooks/useUser.ts
@@ -1,16 +1,11 @@
 import { useSelector } from "react-redux";
 import { ReducerType } from "../../modules";
 
-const useUser = () => {
+function useUser() {
   const { user } = useSelector(({ user }: ReducerType) => ({
     user: user.user,
   }));
   return user;
-};
-
-export const useUserId = () => {
-  const user = useUser();
-  return user && user.id;
-};
+}
 
 export default useUser;
