e84c7b8febb76ba026b910939fe63b74a1e0c5b1
Chickenbumps <whgotjd46@gmail.com>
2022-06-20 21:43:19 +0900

    Feat: User Reducer 추가

--- src/modules/user.ts
+++ src/modules/user.ts
@@ -1,12 +1,19 @@
 import { call, takeLatest } from "redux-saga/effects";
 import { AuthApi } from "../lib/api/auth";
+import UserApi from "../lib/api/user";
 import createRequestSaga from "../lib/createRequestSaga";
 import {
   CHECK,
   CHECK_FAILURE,
   CHECK_SUCCESS,
+  GET_USER_PROFILE,
+  GET_USER_PROFILE_FAILURE,
+  GET_USER_PROFILE_SUCCESS,
   LOGOUT,
   TEMP_SET_USER,
+  UPDATE_USER_PROFILE,
+  UPDATE_USER_PROFILE_FAILURE,
+  UPDATE_USER_PROFILE_SUCCESS,
   UserDispatchType,
   userInitialStateType,
 } from "./user.type";
@@ -20,7 +27,44 @@ export const tempSetUser = (user: any) => ({
 export const check = () => ({ type: CHECK });
 export const logout = () => ({ type: LOGOUT });
 
+export const getUserProfile = (nickname: string) => ({
+  type: GET_USER_PROFILE,
+  payload: {
+    nickname,
+  },
+});
+
+export const updateUserProfile = ({
+  userId,
+  description,
+  thumbnail,
+  password,
+}: {
+  userId: number;
+  description?: string;
+  thumbnail?: string;
+  password?: string;
+}) => ({
+  type: UPDATE_USER_PROFILE,
+  payload: {
+    userId,
+    description,
+    thumbnail,
+    password,
+  },
+});
+
 const checkSaga = createRequestSaga(CHECK, AuthApi.check, "user");
+const getUserProfileSaga = createRequestSaga(
+  GET_USER_PROFILE,
+  UserApi.getUserProfile,
+  "userProfile",
+);
+const updateUserProfileSage = createRequestSaga(
+  UPDATE_USER_PROFILE,
+  UserApi.updateUserProfile,
+  "user",
+);
 
 function* logoutSaga() {
   try {
@@ -43,11 +87,15 @@ export function* userSaga() {
   yield takeLatest(CHECK, checkSaga);
   yield takeLatest(CHECK_FAILURE, checkFailureSaga);
   yield takeLatest(LOGOUT, logoutSaga);
+  yield takeLatest(GET_USER_PROFILE, getUserProfileSaga);
+  yield takeLatest(UPDATE_USER_PROFILE, updateUserProfileSage);
 }
 
 const initialState: userInitialStateType = {
   user: null,
   userError: null,
+  userProfile: null,
+  userProfileError: null,
 };
 
 const user = (
@@ -76,6 +124,26 @@ const user = (
       return {
         ...state,
         user: initialState.user,
+      };
+    case GET_USER_PROFILE_SUCCESS:
+      return {
+        ...state,
+        userProfile: action.payload.userProfile,
+      };
+    case GET_USER_PROFILE_FAILURE:
+      return {
+        ...state,
+        userProfileError: action.payload.userProfileError,
+      };
+    case UPDATE_USER_PROFILE_SUCCESS:
+      return {
+        ...state,
+        user: action.payload.user,
+      };
+    case UPDATE_USER_PROFILE_FAILURE:
+      return {
+        ...state,
+        userError: action.payload.userError,
       };
     default:
       return state;
--- src/modules/user.type.ts
+++ src/modules/user.type.ts
@@ -3,14 +3,27 @@ export const CHECK = "user/CHECK" as const;
 export const CHECK_SUCCESS = "user/CHECK_SUCCESS" as const;
 export const CHECK_FAILURE = "user/CHECK_FAILURE" as const;
 export const LOGOUT = "user/LOGOUT" as const;
+export const GET_USER_PROFILE = "user/GET_USER_PROFILE" as const;
+export const GET_USER_PROFILE_SUCCESS =
+  "user/GET_USER_PROFILE_SUCCESS" as const;
+export const GET_USER_PROFILE_FAILURE =
+  "user/GET_USER_PROFILE_FAILURE" as const;
+export const UPDATE_USER_PROFILE = "user/UPDATE_USER_PROFILE" as const;
+export const UPDATE_USER_PROFILE_SUCCESS =
+  "user/UPDATE_USER_PROFILE_SUCCESS" as const;
+export const UPDATE_USER_PROFILE_FAILURE =
+  "user/UPDATE_USER_PROFILE_FAILURE" as const;
 export interface userInitialStateType {
   user: {
     id: number;
     name: string;
     nickname: string;
     thumbnail?: string;
+    description?: string;
   } | null;
   userError: null;
+  userProfile: any;
+  userProfileError: any;
 }
 
 export interface tempSetUserDispatch {
@@ -53,8 +66,39 @@ export interface logoutDispatch {
   };
 }
 
+export interface getUserProfileSuccessDispatch {
+  type: typeof GET_USER_PROFILE_SUCCESS;
+  payload: {
+    userProfile: any;
+  };
+}
+
+export interface getUserProfileFailureDispatch {
+  type: typeof GET_USER_PROFILE_FAILURE;
+  payload: {
+    userProfileError: any;
+  };
+}
+
+export interface updateUserProfileSuccessDispatch {
+  type: typeof UPDATE_USER_PROFILE_SUCCESS;
+  payload: {
+    user: userInitialStateType["user"];
+  };
+}
+export interface updateUserProfileFailureDispatch {
+  type: typeof UPDATE_USER_PROFILE_FAILURE;
+  payload: {
+    userError: any;
+  };
+}
+
 export type UserDispatchType =
   | tempSetUserDispatch
   | checkSuccessDispatch
   | checkFailureDispatch
-  | logoutDispatch;
+  | logoutDispatch
+  | getUserProfileSuccessDispatch
+  | getUserProfileFailureDispatch
+  | updateUserProfileSuccessDispatch
+  | updateUserProfileFailureDispatch;
