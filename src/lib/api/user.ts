6c5a496cc7e562573988d3798d4e172153544144
Chickenbumps <whgotjd46@gmail.com>
2022-06-20 21:44:18 +0900

    Feat: User API 추가

---
+++ src/lib/api/user.ts
@@ -0,0 +1,30 @@
+import client from "./client";
+import qs from "qs";
+
+class UserApi {
+  static getUserProfile({ nickname }: { nickname: string }) {
+    return client.get(`/api/user/${nickname}`);
+  }
+
+  static updateUserProfile({
+    userId,
+    description,
+    thumbnail,
+    password,
+  }: {
+    userId: number;
+    description: string | undefined;
+    thumbnail: string | undefined;
+    password: string | undefined;
+  }) {
+    const queryString = qs.stringify({
+      userId,
+      description,
+      thumbnail,
+      password,
+    });
+    return client.patch(`api/user?${queryString}`);
+  }
+}
+
+export default UserApi;
