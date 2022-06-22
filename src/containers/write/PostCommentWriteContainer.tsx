ffed173664b0db821f8dd9d43d8a5ce7076c2663
Chickenbumps <whgotjd46@gmail.com>
2022-06-22 14:20:52 +0900

    Feat: 로그인이 필요한 서비스 접근 시 모달창 띄우기
    - 비로그인 상태에서 댓글쓰기, 글쓰기에 접근하면 로그인 모달창 띄우게 설정

--- src/containers/write/PostCommentWriteContainer.tsx
+++ src/containers/write/PostCommentWriteContainer.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
+import AskModal from "../../components/common/AskModal/AskModal";
 import PostCommentWrite from "../../components/post/PostCommentWrite";
 import useInput from "../../lib/hooks/useInput";
 import useUser from "../../lib/hooks/useUser";
@@ -19,25 +20,39 @@ const PostCommentWriteContainer: React.FC<PostCommentWriteProps> = ({
 }) => {
   const [comment, onChange, onReset] = useInput("");
   const dispatch = useDispatch();
-  const naviagtion = useNavigate();
+  const navigation = useNavigate();
   const user = useUser();
   const location = useLocation();
+  const [modal, setModal] = useState(false);
   const onCreateComment = () => {
     if (!user) {
-      return naviagtion("/login", { state: location.pathname });
+      setModal(true);
+    } else {
+      dispatch(createComment(user.id, Number(postId), null, comment));
     }
-    dispatch(createComment(user.id, Number(postId), null, comment));
     onReset();
   };
 
   return (
-    <PostCommentWrite
-      edit={edit}
-      onChange={onChange}
-      onCreate={() => onCreateComment()}
-      add={add}
-      comment={comment}
-    />
+    <>
+      <PostCommentWrite
+        edit={edit}
+        onChange={onChange}
+        onCreate={() => onCreateComment()}
+        add={add}
+        comment={comment}
+      />
+      {modal && (
+        <AskModal
+          title="로그인"
+          visible={true}
+          description="로그인이 필요한 서비스입니다."
+          confirmText="로그인"
+          onCancel={() => setModal(false)}
+          onConfirm={() => navigation("/login", { state: location.pathname })}
+        />
+      )}
+    </>
   );
 };
 
