a53b5784418190af75e11065ccd9aa08d6eee7da
Chickenbumps <whgotjd46@gmail.com>
2022-06-25 18:49:13 +0900

    Feat: searchPostList API 추가
    - post의 title, body, tag에 검색어가 있는 postList 찾는 API 추가

--- src/modules/postList.ts
+++ src/modules/postList.ts
@@ -7,17 +7,29 @@ import {
   READ_POST_LIST,
   READ_POST_LIST_FAILURE,
   READ_POST_LIST_SUCCESS,
+  SEARCH_POST_LIST,
+  SEARCH_POST_LIST_FAILURE,
+  SEARCH_POST_LIST_SUCCESS,
 } from "./postList.type";
 
 export interface queryStringType {
   page?: string | qs.ParsedQs | undefined | number;
   nickname?: string | qs.ParsedQs | undefined;
   tag?: string | qs.ParsedQs | qs.ParsedQs[] | undefined | number;
+  word?: string | qs.ParsedQs | undefined;
 }
 
 export const readPostList = ({ page, nickname, tag }: queryStringType) => ({
   type: READ_POST_LIST,
   payload: { page, nickname, tag },
+});
+
+export const searchPostList = ({ page, word }: queryStringType) => ({
+  type: SEARCH_POST_LIST,
+  payload: {
+    page,
+    word,
+  },
 });
 
 const readPostListSaga = createRequestSaga(
@@ -26,14 +38,24 @@ const readPostListSaga = createRequestSaga(
   "postList",
 );
 
+const searchPostListSaga = createRequestSaga(
+  SEARCH_POST_LIST,
+  PostApi.searchPostList,
+  "searchPostList",
+);
+
 export function* postListSaga() {
   yield takeLatest(READ_POST_LIST, readPostListSaga);
+  yield takeLatest(SEARCH_POST_LIST, searchPostListSaga);
 }
 
 const initialState: postListInitialStateType = {
   postList: null,
+  searchPostList: null,
+  searchPostListError: null,
   postListError: null,
   lastPage: 1,
+  totalPostCount: 0,
 };
 
 const postList = (
@@ -51,6 +73,18 @@ const postList = (
       return {
         ...state,
         postListError: action.payload.postListError,
+      };
+    case SEARCH_POST_LIST_SUCCESS:
+      return {
+        ...state,
+        searchPostList: action.payload.searchPostList,
+        lastPage: parseInt(action.meta.headers["last-page"], 10),
+        totalPostCount: parseInt(action.meta.headers["total-post-count"], 10),
+      };
+    case SEARCH_POST_LIST_FAILURE:
+      return {
+        ...state,
+        searchPostListtError: action.payload.searchPostListError,
       };
     default:
       return state;
