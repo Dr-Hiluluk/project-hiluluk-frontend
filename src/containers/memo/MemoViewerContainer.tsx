feca78157331d1d612ab92d57d50c5f0e2ec6d09
Chickenbumps <whgotjd46@gmail.com>
2022-06-25 19:29:24 +0900

    Feat: memo 삭제시 모달 창 뜨도록 변경

--- src/containers/memo/MemoViewerContainer.tsx
+++ src/containers/memo/MemoViewerContainer.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import AskModal from "../../components/common/AskModal/AskModal";
 import MemoViewer from "../../components/memo/MemoViewer";
 import useUser from "../../lib/hooks/useUser";
 
@@ -20,14 +21,36 @@ const MemoViewerContainer: React.FC<MemoViewerContainerProps> = ({
   const { nickname } = useParams();
   const isMine = nickname === user?.nickname;
 
+  const [modal, setModal] = useState(false);
+  const onDeleteClick = () => {
+    setModal(true);
+  };
+  const onConfirm = () => {
+    setModal(false);
+    onRemove(memo.id);
+  };
+  const onCancel = () => {
+    setModal(false);
+  };
+
   return (
-    <MemoViewer
-      isMine={isMine}
-      memo={memo}
-      date={date}
-      onEdit={onEdit}
-      onRemove={onRemove}
-    />
+    <>
+      <MemoViewer
+        isMine={isMine}
+        memo={memo}
+        date={date}
+        onEdit={onEdit}
+        onRemove={onDeleteClick}
+      />
+      <AskModal
+        visible={modal}
+        title="메모 삭제"
+        description="정말 삭제하시겠습니까?"
+        confirmText="삭제"
+        onConfirm={onConfirm}
+        onCancel={onCancel}
+      />
+    </>
   );
 };
 
