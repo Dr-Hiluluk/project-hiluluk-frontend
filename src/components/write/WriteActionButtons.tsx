77afacefcbd0dc768a28462168d7b918eff20c68
Chickenbumps <whgotjd46@gmail.com>
2022-09-01 15:09:21 +0900

    Feat: 추가 글 임시 저장 버튼

--- src/components/write/WriteActionButtons.tsx
+++ src/components/write/WriteActionButtons.tsx
@@ -4,7 +4,7 @@ import "./WriteActionButtons.scss";
 
 interface writeActionButtonsType {
   onCancel: React.MouseEventHandler<HTMLButtonElement> | undefined;
-  onPublish: React.MouseEventHandler<HTMLButtonElement> | undefined;
+  onPublish: (isTemp?: boolean) => void;
   isEdit: boolean;
 }
 
@@ -15,7 +15,10 @@ const WriteActionButtons = ({
 }: writeActionButtonsType) => {
   return (
     <div className="write-action-button-block">
-      <Button onClick={onPublish} teal>
+      <span className="temp-save-button" onClick={() => onPublish(true)}>
+        임시 저장
+      </span>
+      <Button onClick={() => onPublish()} teal>
         게시글 {isEdit ? "수정" : "등록"}
       </Button>
       <Button onClick={onCancel}>취소</Button>
